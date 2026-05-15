ced19b02251ead7a1db333cea3de00d3c2b822b8
whale2200d <whale2200d@gmail.com>
2026-05-15 22:39:00 +0900

    fix: callback 공통 함수 추가

--- afo/cli.py
+++ afo/cli.py
@@ -9,9 +9,11 @@ app = typer.Typer(
 
 console = Console()
 
+# 프로그램 시작할 때 제일 먼저 실행할 공통 함수
 @app.callback()
 def callback():
     """Auto File Organizer 시작"""
+    
     pass
 
 # TODO: 나중에 명령어를 여기에 추가 예정
