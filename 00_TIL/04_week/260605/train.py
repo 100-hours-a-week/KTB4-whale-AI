0c97ce8e798fa93eafab6b598c78556b2def64d4
whale2200d <whale2200d@gmail.com>
2026-06-06 14:53:08 +0900

    fix: 불필요한 파일 제거 및 weights, bias 누적 연산 로직 수정

--- 00_TIL/04_week/260605/train.py
+++ 00_TIL/04_week/260605/train.py
@@ -1,5 +1,5 @@
-from train_batch import train_batch 
 from types import SimpleNamespace
+from train_perceptron import train_perceptron
 
 def train(inputs, outputs, epochs, weights, bias, learning_rate, activate_f):
     """
@@ -9,23 +9,34 @@ def train(inputs, outputs, epochs, weights, bias, learning_rate, activate_f):
     """
     # 2. 모델 훈련
     # 2.1. 모델 훈련
-    worked_batch = None
+    current_weights = weights.copy()
+    current_bias = bias.copy()
 
     for epoch in range(epochs):
         print(f"\n{'='*60}")
         print(f"Epoch {epoch + 1} / {epochs}")
         print(f"{'='*60}")
 
-        worked_batch = train_batch(inputs, outputs, weights, bias, learning_rate, activate_f)
+        for index in range(len(inputs)):
+            perceptron = train_perceptron(
+                index=index,
+                inputs=inputs,
+                outputs=outputs,
+                weights=current_weights,
+                bias=current_bias,
+                learning_rate=learning_rate,
+                activate_f=activate_f
+            )
 
-    assert worked_batch is not None # "이 변수는 None이 아니다"라고 알려줌
+            current_weights = perceptron.weights
+            current_bias = perceptron.bias
 
     # 2.2. 훈련 결과 확인
     print("\n ==== 학습 종료 ====")
-    print("학습된 가중치:", worked_batch.weights)
-    print("학습된 편향:", worked_batch.bias)
+    print("학습된 가중치:", current_weights)
+    print("학습된 편향:", current_bias)
 
     return SimpleNamespace(
-        weights=worked_batch.weights,
-        bias=worked_batch.bias
+        weights=current_weights,
+        bias=current_bias
     )
